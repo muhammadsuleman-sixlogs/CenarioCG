1832ee263ba3db047a9282ca9b0ded8c4407e2b3
Muhammad Suleman Nagri <muhammad.suleman@sixlogs.com>
2026-09-17 17:06:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from api.chat import router as chat_router
 
 
@@ -9,7 +8,11 @@ app = FastAPI(title="AI Context Layer API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://cenario-cg.vercel.app","https://cenario-cg-xmvw.vercel.app/"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://cenario-cg.vercel.app",
+        "https://cenario-cg-xmvw.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
